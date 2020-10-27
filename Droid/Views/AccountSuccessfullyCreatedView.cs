0fdad81ab0e101b68b0c92b3993147d33945e0fb
speet1234 <sundeep.peethani@gmail.com>
2020-10-27 15:34:11 -0700

    Added missing files in git

---
+++ Droid/Views/AccountSuccessfullyCreatedView.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+using SpectrumCC.ViewModels;
+
+namespace SpectrumCC.Droid.Views
+{
+    [Activity(Label = "AccountSuccessfullyCreatedView")]
+    public class AccountSuccessfullyCreatedView : MvxActivity<AccountSuccessfullyCreatedViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.AccountSuccessfullyCreatedView);
+            // Create your application here
+        }
+    }
+}
---
+++ SpectrumCC/ViewModels/AccountSuccessfullyCreatedViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+
+namespace SpectrumCC.ViewModels
+{
+    public class AccountSuccessfullyCreatedViewModel: MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+
+        public AccountSuccessfullyCreatedViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public IMvxCommand LoginNowCommand => new MvxCommand(LoginNow);
+        private void LoginNow()
+        {
+            _navigationService.Navigate<MainViewModel>();
+        }
+    }
+}
